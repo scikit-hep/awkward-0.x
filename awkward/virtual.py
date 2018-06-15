4fbdcc061c85b0736f74477d19227e7103440c5d
Jim Pivarski <jpivarski@gmail.com>
2018-06-15 20:01:43 +0200

    VirtualArray should be able to talk to caches

--- awkward/virtual.py
+++ awkward/virtual.py
@@ -35,8 +35,23 @@ import numpy
 import awkward.base
 
 class VirtualArray(awkward.base.AwkwardArray):
-    def __init__(self, generator, dtype=None, shape=None):
+    class Key(object):
+        def __init__(self, id):
+            self._id = id
+        def __repr__(self):
+            return "<VirtualArray.Key {0}>".format(self._id)
+        def __hash__(self):
+            return hash((VirtualArray.Key, self._id))
+        def __eq__(self, other):
+            return isinstance(other, VirtualArray.Key) and self._id == other._id
+        def __ne__(self, other):
+            return not self.__eq__(other)
+        def __getstate__(self):
+            raise RuntimeError("VirtualArray.Keys are not unique across processes, and hence should not be serialized")
+
+    def __init__(self, generator, cache=None, dtype=None, shape=None):
         self.generator = generator
+        self.cache = cache
         self._array = None
 
         if dtype is None:
@@ -49,19 +64,25 @@ class VirtualArray(awkward.base.AwkwardArray):
         else:
             raise TypeError("shape must be None (unknown) or a non-empty tuple of non-negative integers")
 
-    def materialize(self):
-        array = self.generator()
-        if self._dtype is not None and self._dtype != array.dtype:
-            raise ValueError("materialized array has dtype {0}, expected dtype {1}".format(array.dtype, self._dtype))
-        if self._shape is not None and self._shape != array.shape:
-            raise ValueError("materialized array has shape {0}, expected shape {1}".format(array.shape, self._shape))
-        if len(array.shape) == 0:
-            raise ValueError("materialized object is scalar: {0}".format(array))
-        self._array, self._dtype, self._shape = array, array.dtype, array.shape
+    @property
+    def generator(self):
+        return self._generator
 
-    def ensure_materialized(self):
-        if self._array is None:
-            self.materialize()
+    @generator.setter
+    def generator(self, value):
+        if not callable(value):
+            raise TypeError("generator must be a callable (of zero arguments)")
+        self._generator = value
+
+    @property
+    def cache(self):
+        return self._cache
+
+    @cache.setter
+    def cache(self, value):
+        if not callable(getattr(value, "__getitem__", None)) or not callable(getattr(value, "__setitem__", None)) or not callable(getattr(value, "__delitem__", None)):
+            raise TypeError("cache must be a dict or have __getitem__/__setitem__/__delitem__ methods")
+        self._cache = value
 
     @property
     def dtype(self):
@@ -70,19 +91,86 @@ class VirtualArray(awkward.base.AwkwardArray):
     @property
     def shape(self):
         return self._shape
-    
+
+    @property
+    def key(self):
+        return self.Key(id(self))
+
+    @property
+    def array(self):
+        # Normal states:   (1) no cache and _array is None: make a new one
+        #                  (2) no cache and _array is an array: return _array
+        #                  (3) have a cache and _array is None: make a new one (filling cache)
+        #                  (4) have a cache and _array is a key and cache[key] was evicted: make a new one (filling cache)
+        #                  (5) have a cache and _array is a key and cache[key] exists: return cache[key]
+        # 
+        # Abnormal states: (6) no cache and _array is a key (user removed _cache): make a new one
+        #                  (7) have a cache and _array is an array (user added _cache): fill cache and return _array
+
+        something = self._array
+
+        if something is None:
+            # states (1) and (3)
+            return self.materialize()
+
+        elif self._cache is None:
+            if isinstance(something, VirtualArray.Key):
+                # abnormal state (6)
+                return self.materialize()
+            else:
+                # state (2)
+                return something
+
+        else:
+            if isinstance(something, VirtualArray.Key):
+                try:
+                    # state (5)
+                    return self._cache[something]
+                except:
+                    # state (4), taking any error in __getitem__ as evidence that it was evicted
+                    return self.materialize()
+            else:
+                # abnormal state (7)
+                self._cache[self.key] = something
+                return something
+
+    def materialize(self):
+        array = self.generator()
+        if self._dtype is not None and self._dtype != array.dtype:
+            raise ValueError("materialized array has dtype {0}, expected dtype {1}".format(array.dtype, self._dtype))
+        if self._shape is not None and self._shape != array.shape:
+            raise ValueError("materialized array has shape {0}, expected shape {1}".format(array.shape, self._shape))
+        if len(array.shape) == 0:
+            raise ValueError("materialized object is scalar: {0}".format(array))
+
+        if self._cache is None:
+            # states (1), (2), and (6)
+            self._array, self._dtype, self._shape = array, array.dtype, array.shape
+        else:
+            # states (3) and (4)
+            self._array = self.key
+            self._cache[self._array], self._dtype, self._shape = array, array.dtype, array.shape
+
+        return array
+
+    def __del__(self):
+        # after this object is garbage collected, its id may be reused: remove its key (based on id) from the cache
+        if self._cache is not None and isinstance(self._array, VirtualArray.Key):
+            try:
+                del self._cache[self._array]
+            except:
+                pass
+
     def __len__(self):
         if self._shape is None:
-            self.ensure_materialized()
+            self.materialize()
         return self._shape[0]
 
     def __getitem__(self, where):
-        self.ensure_materialized()
-        return self._array[where]
+        return self.array[where]
 
     def __setitem__(self, where, what):
-        self.ensure_materialized()
-        self._array[where] = what
+        self.array[where] = what
         
 class VirtualObjectArray(awkward.base.AwkwardArray):
     pass
