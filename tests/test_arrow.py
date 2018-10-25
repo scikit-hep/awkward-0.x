680a7f8aede85e3d044437c8b44054463a6036e9
Jim Pivarski <jpivarski@gmail.com>
2018-10-25 16:00:53 -0500

    Arrow test suite done, too

--- tests/test_arrow.py
+++ tests/test_arrow.py
@@ -36,6 +36,7 @@ try:
 except ImportError:
     pyarrow = None
 
+import awkward.arrow
 from awkward import *
 
 class Test(unittest.TestCase):
@@ -44,165 +45,203 @@ class Test(unittest.TestCase):
 
     def test_array(self):
         if pyarrow is not None:
-            arr = pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5])
+            a = pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5])
+            assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, 4.4, 5.5]
 
     def test_boolean(self):
         if pyarrow is not None:
-            arr = pyarrow.array([True, True, False, False, True])
+            a = pyarrow.array([True, True, False, False, True])
+            assert awkward.arrow.view(a).tolist() == [True, True, False, False, True]
 
     def test_array_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([1.1, 2.2, 3.3, None, 4.4, 5.5])
+            a = pyarrow.array([1.1, 2.2, 3.3, None, 4.4, 5.5])
+            assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, None, 4.4, 5.5]
 
     def test_nested_array(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[1.1, 2.2, 3.3], [], [4.4, 5.5]])
+            a = pyarrow.array([[1.1, 2.2, 3.3], [], [4.4, 5.5]])
+            assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, 3.3], [], [4.4, 5.5]]
 
     def test_nested_array_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[1.1, 2.2, None], [], [4.4, 5.5]])
+            a = pyarrow.array([[1.1, 2.2, None], [], [4.4, 5.5]])
+            assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, None], [], [4.4, 5.5]]
 
     def test_null_nested_array_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[1.1, 2.2, None], [], None, [4.4, 5.5]])
+            a = pyarrow.array([[1.1, 2.2, None], [], None, [4.4, 5.5]])
+            assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, None], [], None, [4.4, 5.5]]
 
     def test_chunked_array(self):
         if pyarrow is not None:
-            arr = pyarrow.chunked_array([pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5]), pyarrow.array([]), pyarrow.array([6.6, 7.7, 8.8])])
+            a = pyarrow.chunked_array([pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5]), pyarrow.array([]), pyarrow.array([6.6, 7.7, 8.8])])
+            assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8]
 
     def test_struct(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
+            a = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]
 
     def test_struct_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
+            a = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}]
 
     def test_null_struct(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
+            a = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]
 
     def test_null_struct_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
+            a = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, None, {"x": 2, "y": None}, {"x": 3, "y": 3.3}]
 
     def test_chunked_struct(self):
         if pyarrow is not None:
-            arr = pyarrow.chunked_array([pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]), pyarrow.array([]), pyarrow.array([{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}])])
+            a = pyarrow.chunked_array([pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]), pyarrow.array([]), pyarrow.array([{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}])])
+            assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}, {"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]
 
     def test_nested_struct(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
     def test_nested_struct_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
     def test_null_nested_struct(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
     def test_null_nested_struct_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
     def test_struct_nested(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}])
+            a = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}]
 
     def test_struct_nested_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}])
+            a = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}])
+            assert awkward.arrow.view(a).tolist() == [{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}]
 
     def test_nested_struct_nested(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}], [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
+            a = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}], [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}], [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]]
 
     def test_null_nested_struct_nested_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}], None, [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
+            a = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}], None, [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
+            assert awkward.arrow.view(a).tolist() == [[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}], None, [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]]
 
     def test_strings(self):
         if pyarrow is not None:
-            arr = pyarrow.array(["one", "two", "three", u"fo\u2014ur", "five"])
+            a = pyarrow.array(["one", "two", "three", u"fo\u2014ur", "five"])
+            assert awkward.arrow.view(a).tolist() == ["one", "two", "three", u"fo\u2014ur", "five"]
 
     def test_strings_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array(["one", "two", None, u"fo\u2014ur", "five"])
+            a = pyarrow.array(["one", "two", None, u"fo\u2014ur", "five"])
+            assert awkward.arrow.view(a).tolist() == ["one", "two", None, u"fo\u2014ur", "five"]
 
     def test_binary(self):
         if pyarrow is not None:
-            arr = pyarrow.array([b"one", b"two", b"three", b"four", b"five"])
+            a = pyarrow.array([b"one", b"two", b"three", b"four", b"five"])
+            assert awkward.arrow.view(a).tolist() == [b"one", b"two", b"three", b"four", b"five"]
 
     def test_binary_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([b"one", b"two", None, b"four", b"five"])
+            a = pyarrow.array([b"one", b"two", None, b"four", b"five"])
+            assert awkward.arrow.view(a).tolist() == [b"one", b"two", None, b"four", b"five"]
 
     def test_chunked_strings(self):
         if pyarrow is not None:
-            arr = pyarrow.chunked_array([pyarrow.array(["one", "two", "three", "four", "five"]), pyarrow.array(["six", "seven", "eight"])])
+            a = pyarrow.chunked_array([pyarrow.array(["one", "two", "three", "four", "five"]), pyarrow.array(["six", "seven", "eight"])])
+            assert awkward.arrow.view(a).tolist() == ["one", "two", "three", "four", "five", "six", "seven", "eight"]
 
     def test_nested_strings(self):
         if pyarrow is not None:
-            arr = pyarrow.array([["one", "two", "three"], [], ["four", "five"]])
+            a = pyarrow.array([["one", "two", "three"], [], ["four", "five"]])
+            assert awkward.arrow.view(a).tolist() == [["one", "two", "three"], [], ["four", "five"]]
 
     def test_nested_strings_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([["one", "two", None], [], ["four", "five"]])
+            a = pyarrow.array([["one", "two", None], [], ["four", "five"]])
+            assert awkward.arrow.view(a).tolist() == [["one", "two", None], [], ["four", "five"]]
 
     def test_null_nested_strings_null(self):
         if pyarrow is not None:
-            arr = pyarrow.array([["one", "two", None], [], None, ["four", "five"]])
+            a = pyarrow.array([["one", "two", None], [], None, ["four", "five"]])
+            assert awkward.arrow.view(a).tolist() == [["one", "two", None], [], None, ["four", "five"]]
 
     def test_union_sparse(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, 2.2, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
+            a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, 2.2, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, True, 2.2, 3.3, False]
 
     def test_union_sparse_null(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
+            a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, True, None, 3.3, False]
 
     def test_union_sparse_null_null(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, None, False, True, False])])
+            a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, None, False, True, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, None, None, 3.3, False]
 
     def test_union_dense(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, 2.2, 3.3]), pyarrow.array([True, True, False])])
+            a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, 2.2, 3.3]), pyarrow.array([True, True, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, 2.2, 3.3, True, False]
 
     def test_union_dense_null(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, True, False])])
+            a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, True, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, None, 3.3, True, False]
 
     def test_union_dense_null_null(self):
         if pyarrow is not None:
-            arr = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, None, False])])
+            a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, None, False])])
+            assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, None, 3.3, None, False]
 
     def test_dictarray(self):
         if pyarrow is not None:
-            arr = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
+            a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
+            assert awkward.arrow.view(a).tolist() == ["one", "one", "three", "three", "two", "one", "three", "two", "two"]
 
     def test_dictarray_null(self):
         if pyarrow is not None:
-            arr = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, None, 1, None, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
+            a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, None, 1, None, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
+            assert awkward.arrow.view(a).tolist() == ["one", "one", "three", None, "two", None, "three", "two", "two"]
 
     def test_null_dictarray(self):
         if pyarrow is not None:
-            arr = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", None, "three"]))
+            a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", None, "three"]))
+            assert awkward.arrow.view(a).tolist() == ["one", "one", "three", "three", None, "one", "three", None, None]
 
     def test_batch(self):
         if pyarrow is not None:
-            arr = pyarrow.RecordBatch.from_arrays(
+            a = pyarrow.RecordBatch.from_arrays(
                 [pyarrow.array([1.1, 2.2, 3.3, None, 5.5]),
                  pyarrow.array([[1, 2, 3], [], [4, 5], [None], [6]]),
                  pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}, {"x": 4, "y": None}, {"x": 5, "y": 5.5}]),
                  pyarrow.array([{"x": 1, "y": 1.1}, None, None, {"x": 4, "y": None}, {"x": 5, "y": 5.5}]),
                  pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": None}, {"x": 5, "y": 5.5}], [None], [{"x": 6, "y": 6.6}]])],
                 ["a", "b", "c", "d", "e"])
+            assert awkward.arrow.view(a).tolist() == [{"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d":{"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}]
 
     def test_table(self):
         if pyarrow is not None:
-            arr = pyarrow.Table.from_batches([
+            a = pyarrow.Table.from_batches([
                 pyarrow.RecordBatch.from_arrays(
                 [pyarrow.array([1.1, 2.2, 3.3, None, 5.5]),
                  pyarrow.array([[1, 2, 3], [], [4, 5], [None], [6]]),
@@ -217,3 +256,4 @@ class Test(unittest.TestCase):
                  pyarrow.array([{"x": 1, "y": 1.1}, None, None, {"x": 4, "y": None}, {"x": 5, "y": 5.5}]),
                  pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": None}, {"x": 5, "y": 5.5}], [None], [{"x": 6, "y": 6.6}]])],
                 ["a", "b", "c", "d", "e"])])
+            assert awkward.arrow.view(a).tolist() == [{"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d": {"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}, {"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d": {"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}]
