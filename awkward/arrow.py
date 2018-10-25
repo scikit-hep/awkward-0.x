01c015eebfcbc4d22212122b150308742347550e
Jim Pivarski <jpivarski@gmail.com>
2018-10-25 17:29:39 -0500

    handle non-nullable cases

--- awkward/arrow.py
+++ awkward/arrow.py
@@ -44,74 +44,114 @@ ARROW_CHARTYPE = awkward.util.numpy.uint8
 def view(obj):
     import pyarrow
 
-    def recurse(tpe, buffers):
+    def popbuffers(tpe, buffers):
         if isinstance(tpe, pyarrow.lib.DictionaryType):
             content = view(tpe.dictionary)
-            index = recurse(tpe.index_type, buffers)
-            assert isinstance(index, awkward.array.masked.BitMaskedArray)
-            return awkward.array.masked.BitMaskedArray(index.mask, awkward.array.indexed.IndexedArray(index.content, content), maskedwhen=index.maskedwhen, lsborder=index.lsborder)
+            index = popbuffers(tpe.index_type, buffers)
+            if isinstance(index, awkward.array.masked.BitMaskedArray):
+                return awkward.array.masked.BitMaskedArray(index.mask, awkward.array.indexed.IndexedArray(index.content, content), maskedwhen=index.maskedwhen, lsborder=index.lsborder)
+            else:
+                return awkward.array.indexed.IndexedArray(index, content)
 
         elif isinstance(tpe, pyarrow.lib.StructType):
             pairs = []
             for i in range(tpe.num_children - 1, -1, -1):
-                pairs.insert(0, (tpe[i].name, recurse(tpe[i].type, buffers)))
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.array.table.Table.frompairs(pairs), maskedwhen=False, lsborder=True)
+                pairs.insert(0, (tpe[i].name, popbuffers(tpe[i].type, buffers)))
+            out = awkward.array.table.Table.frompairs(pairs)
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif isinstance(tpe, pyarrow.lib.ListType):
-            content = recurse(tpe.value_type, buffers)
+            content = popbuffers(tpe.value_type, buffers)
             offsets = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_INDEXTYPE)
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.array.jagged.JaggedArray.fromoffsets(offsets, content), maskedwhen=False, lsborder=True)
+            out = awkward.array.jagged.JaggedArray.fromoffsets(offsets, content)
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif isinstance(tpe, pyarrow.lib.UnionType) and tpe.mode == "sparse":
             contents = []
             for i in range(tpe.num_children - 1, -1, -1):
-                contents.insert(0, recurse(tpe[i].type, buffers))
+                contents.insert(0, popbuffers(tpe[i].type, buffers))
             assert buffers.pop() is None
             tags = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_TAGTYPE)
             index = awkward.util.numpy.arange(len(tags), dtype=ARROW_INDEXTYPE)
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.array.union.UnionArray(tags, index, contents), maskedwhen=False, lsborder=True)
+            out = awkward.array.union.UnionArray(tags, index, contents)
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif isinstance(tpe, pyarrow.lib.UnionType) and tpe.mode == "dense":
             contents = []
             for i in range(tpe.num_children - 1, -1, -1):
-                contents.insert(0, recurse(tpe[i].type, buffers))
+                contents.insert(0, popbuffers(tpe[i].type, buffers))
             index = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_INDEXTYPE)
             tags = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_TAGTYPE)
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.array.union.UnionArray(tags, index, contents), maskedwhen=False, lsborder=True)
+            out = awkward.array.union.UnionArray(tags, index, contents)
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif tpe == pyarrow.string():
             content = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_CHARTYPE)
             offsets = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_INDEXTYPE)
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.derived.strings.StringArray.fromoffsets(offsets, content, encoding="utf-8"), maskedwhen=False, lsborder=True)
+            out = awkward.derived.strings.StringArray.fromoffsets(offsets, content, encoding="utf-8")
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif tpe == pyarrow.binary():
             content = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_CHARTYPE)
             offsets = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_INDEXTYPE)
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, awkward.derived.strings.StringArray.fromoffsets(offsets, content, encoding=None), maskedwhen=False, lsborder=True)
+            out = awkward.derived.strings.StringArray.fromoffsets(offsets, content, encoding=None)
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif tpe == pyarrow.bool_():
-            content = awkward.util.numpy.unpackbits(awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_CHARTYPE)).view(awkward.util.BOOLTYPE)
-            content = content.reshape(-1, 8)[:,::-1].reshape(-1)    # lsborder=True
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, content, maskedwhen=False, lsborder=True)
+            out = awkward.util.numpy.unpackbits(awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_CHARTYPE)).view(awkward.util.BOOLTYPE)
+            out = out.reshape(-1, 8)[:,::-1].reshape(-1)    # lsborder=True
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         elif isinstance(tpe, pyarrow.lib.DataType):
-            content = awkward.util.numpy.frombuffer(buffers.pop(), dtype=tpe.to_pandas_dtype())
-            mask = awkward.util.numpy.frombuffer(buffers.pop(), dtype=ARROW_BITMASKTYPE)
-            return awkward.array.masked.BitMaskedArray(mask, content, maskedwhen=False, lsborder=True)
+            out = awkward.util.numpy.frombuffer(buffers.pop(), dtype=tpe.to_pandas_dtype())
+            mask = buffers.pop()
+            if mask is not None:
+                mask = awkward.util.numpy.frombuffer(mask, dtype=ARROW_BITMASKTYPE)
+                return awkward.array.masked.BitMaskedArray(mask, out, maskedwhen=False, lsborder=True)
+            else:
+                return out
 
         else:
             raise NotImplementedError(repr(tpe))
 
     if isinstance(obj, pyarrow.lib.Array):
         buffers = obj.buffers()
-        out = recurse(obj.type, buffers)[:len(obj)]
+        out = popbuffers(obj.type, buffers)[:len(obj)]
         assert len(buffers) == 0
         return out
 
--- tests/test_arrow.py
+++ tests/test_arrow.py
@@ -33,6 +33,7 @@ import unittest
 import numpy
 try:
     import pyarrow
+    import pyarrow.parquet
 except ImportError:
     pyarrow = None
 
@@ -43,192 +44,192 @@ class Test(unittest.TestCase):
     def runTest(self):
         pass
 
-    def test_array(self):
+    def test_arrow_array(self):
         if pyarrow is not None:
             a = pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5])
             assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, 4.4, 5.5]
 
-    def test_boolean(self):
+    def test_arrow_boolean(self):
         if pyarrow is not None:
             a = pyarrow.array([True, True, False, False, True])
             assert awkward.arrow.view(a).tolist() == [True, True, False, False, True]
 
-    def test_array_null(self):
+    def test_arrow_array_null(self):
         if pyarrow is not None:
             a = pyarrow.array([1.1, 2.2, 3.3, None, 4.4, 5.5])
             assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, None, 4.4, 5.5]
 
-    def test_nested_array(self):
+    def test_arrow_nested_array(self):
         if pyarrow is not None:
             a = pyarrow.array([[1.1, 2.2, 3.3], [], [4.4, 5.5]])
             assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, 3.3], [], [4.4, 5.5]]
 
-    def test_nested_array_null(self):
+    def test_arrow_nested_array_null(self):
         if pyarrow is not None:
             a = pyarrow.array([[1.1, 2.2, None], [], [4.4, 5.5]])
             assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, None], [], [4.4, 5.5]]
 
-    def test_null_nested_array_null(self):
+    def test_arrow_null_nested_array_null(self):
         if pyarrow is not None:
             a = pyarrow.array([[1.1, 2.2, None], [], None, [4.4, 5.5]])
             assert awkward.arrow.view(a).tolist() == [[1.1, 2.2, None], [], None, [4.4, 5.5]]
 
-    def test_chunked_array(self):
+    def test_arrow_chunked_array(self):
         if pyarrow is not None:
             a = pyarrow.chunked_array([pyarrow.array([1.1, 2.2, 3.3, 4.4, 5.5]), pyarrow.array([]), pyarrow.array([6.6, 7.7, 8.8])])
             assert awkward.arrow.view(a).tolist() == [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8]
 
-    def test_struct(self):
+    def test_arrow_struct(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]
 
-    def test_struct_null(self):
+    def test_arrow_struct_null(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}]
 
-    def test_null_struct(self):
+    def test_arrow_null_struct(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]
 
-    def test_null_struct_null(self):
+    def test_arrow_null_struct_null(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": 1, "y": 1.1}, None, {"x": 2, "y": None}, {"x": 3, "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, None, {"x": 2, "y": None}, {"x": 3, "y": 3.3}]
 
-    def test_chunked_struct(self):
+    def test_arrow_chunked_struct(self):
         if pyarrow is not None:
             a = pyarrow.chunked_array([pyarrow.array([{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]), pyarrow.array([]), pyarrow.array([{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}])])
             assert awkward.arrow.view(a).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}, {"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]
 
-    def test_nested_struct(self):
+    def test_arrow_nested_struct(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
-    def test_nested_struct_null(self):
+    def test_arrow_nested_struct_null(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
-    def test_null_nested_struct(self):
+    def test_arrow_null_nested_struct(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
-    def test_null_nested_struct_null(self):
+    def test_arrow_null_nested_struct_null(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": 1, "y": 1.1}, {"x": 2, "y": None}, {"x": 3, "y": 3.3}], None, [], [{"x": 4, "y": 4.4}, {"x": 5, "y": 5.5}]]
 
-    def test_struct_nested(self):
+    def test_arrow_struct_nested(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}]
 
-    def test_struct_nested_null(self):
+    def test_arrow_struct_nested_null(self):
         if pyarrow is not None:
             a = pyarrow.array([{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}])
             assert awkward.arrow.view(a).tolist() == [{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}]
 
-    def test_nested_struct_nested(self):
+    def test_arrow_nested_struct_nested(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}], [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [3, 3], "y": 3.3}], [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]]
 
-    def test_null_nested_struct_nested_null(self):
+    def test_arrow_null_nested_struct_nested_null(self):
         if pyarrow is not None:
             a = pyarrow.array([[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}], None, [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]])
             assert awkward.arrow.view(a).tolist() == [[{"x": [], "y": 1.1}, {"x": [2], "y": 2.2}, {"x": [None, 3], "y": 3.3}], None, [], [{"x": [4, 4, 4], "y": 4.4}, {"x": [5, 5, 5, 5], "y": 5.5}]]
 
-    def test_strings(self):
+    def test_arrow_strings(self):
         if pyarrow is not None:
             a = pyarrow.array(["one", "two", "three", u"fo\u2014ur", "five"])
             assert awkward.arrow.view(a).tolist() == ["one", "two", "three", u"fo\u2014ur", "five"]
 
-    def test_strings_null(self):
+    def test_arrow_strings_null(self):
         if pyarrow is not None:
             a = pyarrow.array(["one", "two", None, u"fo\u2014ur", "five"])
             assert awkward.arrow.view(a).tolist() == ["one", "two", None, u"fo\u2014ur", "five"]
 
-    def test_binary(self):
+    def test_arrow_binary(self):
         if pyarrow is not None:
             a = pyarrow.array([b"one", b"two", b"three", b"four", b"five"])
             assert awkward.arrow.view(a).tolist() == [b"one", b"two", b"three", b"four", b"five"]
 
-    def test_binary_null(self):
+    def test_arrow_binary_null(self):
         if pyarrow is not None:
             a = pyarrow.array([b"one", b"two", None, b"four", b"five"])
             assert awkward.arrow.view(a).tolist() == [b"one", b"two", None, b"four", b"five"]
 
-    def test_chunked_strings(self):
+    def test_arrow_chunked_strings(self):
         if pyarrow is not None:
             a = pyarrow.chunked_array([pyarrow.array(["one", "two", "three", "four", "five"]), pyarrow.array(["six", "seven", "eight"])])
             assert awkward.arrow.view(a).tolist() == ["one", "two", "three", "four", "five", "six", "seven", "eight"]
 
-    def test_nested_strings(self):
+    def test_arrow_nested_strings(self):
         if pyarrow is not None:
             a = pyarrow.array([["one", "two", "three"], [], ["four", "five"]])
             assert awkward.arrow.view(a).tolist() == [["one", "two", "three"], [], ["four", "five"]]
 
-    def test_nested_strings_null(self):
+    def test_arrow_nested_strings_null(self):
         if pyarrow is not None:
             a = pyarrow.array([["one", "two", None], [], ["four", "five"]])
             assert awkward.arrow.view(a).tolist() == [["one", "two", None], [], ["four", "five"]]
 
-    def test_null_nested_strings_null(self):
+    def test_arrow_null_nested_strings_null(self):
         if pyarrow is not None:
             a = pyarrow.array([["one", "two", None], [], None, ["four", "five"]])
             assert awkward.arrow.view(a).tolist() == [["one", "two", None], [], None, ["four", "five"]]
 
-    def test_union_sparse(self):
+    def test_arrow_union_sparse(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, 2.2, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, True, 2.2, 3.3, False]
 
-    def test_union_sparse_null(self):
+    def test_arrow_union_sparse_null(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, True, False, True, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, True, None, 3.3, False]
 
-    def test_union_sparse_null_null(self):
+    def test_arrow_union_sparse_null_null(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_sparse(pyarrow.array([0, 1, 0, 0, 1], type=pyarrow.int8()), [pyarrow.array([0.0, 1.1, None, 3.3, 4.4]), pyarrow.array([True, None, False, True, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, None, None, 3.3, False]
 
-    def test_union_dense(self):
+    def test_arrow_union_dense(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, 2.2, 3.3]), pyarrow.array([True, True, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, 2.2, 3.3, True, False]
 
-    def test_union_dense_null(self):
+    def test_arrow_union_dense_null(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, True, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, None, 3.3, True, False]
 
-    def test_union_dense_null_null(self):
+    def test_arrow_union_dense_null_null(self):
         if pyarrow is not None:
             a = pyarrow.UnionArray.from_dense(pyarrow.array([0, 1, 0, 0, 0, 1, 1], type=pyarrow.int8()), pyarrow.array([0, 0, 1, 2, 3, 1, 2], type=pyarrow.int32()), [pyarrow.array([0.0, 1.1, None, 3.3]), pyarrow.array([True, None, False])])
             assert awkward.arrow.view(a).tolist() == [0.0, True, 1.1, None, 3.3, None, False]
 
-    def test_dictarray(self):
+    def test_arrow_dictarray(self):
         if pyarrow is not None:
             a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
             assert awkward.arrow.view(a).tolist() == ["one", "one", "three", "three", "two", "one", "three", "two", "two"]
 
-    def test_dictarray_null(self):
+    def test_arrow_dictarray_null(self):
         if pyarrow is not None:
             a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, None, 1, None, 2, 1, 1]), pyarrow.array(["one", "two", "three"]))
             assert awkward.arrow.view(a).tolist() == ["one", "one", "three", None, "two", None, "three", "two", "two"]
 
-    def test_null_dictarray(self):
+    def test_arrow_null_dictarray(self):
         if pyarrow is not None:
             a = pyarrow.DictionaryArray.from_arrays(pyarrow.array([0, 0, 2, 2, 1, 0, 2, 1, 1]), pyarrow.array(["one", None, "three"]))
             assert awkward.arrow.view(a).tolist() == ["one", "one", "three", "three", None, "one", "three", None, None]
 
-    def test_batch(self):
+    def test_arrow_batch(self):
         if pyarrow is not None:
             a = pyarrow.RecordBatch.from_arrays(
                 [pyarrow.array([1.1, 2.2, 3.3, None, 5.5]),
@@ -239,7 +240,7 @@ class Test(unittest.TestCase):
                 ["a", "b", "c", "d", "e"])
             assert awkward.arrow.view(a).tolist() == [{"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d":{"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}]
 
-    def test_table(self):
+    def test_arrow_table(self):
         if pyarrow is not None:
             a = pyarrow.Table.from_batches([
                 pyarrow.RecordBatch.from_arrays(
@@ -257,3 +258,25 @@ class Test(unittest.TestCase):
                  pyarrow.array([[{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}], [], [{"x": 4, "y": None}, {"x": 5, "y": 5.5}], [None], [{"x": 6, "y": 6.6}]])],
                 ["a", "b", "c", "d", "e"])])
             assert awkward.arrow.view(a).tolist() == [{"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d": {"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}, {"a": 1.1, "b": [1, 2, 3], "c": {"x": 1, "y": 1.1}, "d": {"x": 1, "y": 1.1}, "e": [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]}, {"a": 2.2, "b": [], "c": {"x": 2, "y": 2.2}, "d": None, "e": []}, {"a": 3.3, "b": [4, 5], "c": {"x": 3, "y": 3.3}, "d": None, "e": [{"x": 4, "y": None}, {"x": 5, "y": 5.5}]}, {"a": None, "b": [None], "c": {"x": 4, "y": None}, "d": {"x": 4, "y": None}, "e": [None]}, {"a": 5.5, "b": [6], "c": {"x": 5, "y": 5.5}, "d": {"x": 5, "y": 5.5}, "e": [{"x": 6, "y": 6.6}]}]
+
+    def test_arrow_nonnullable_table(self):
+        if pyarrow is not None:
+            x = pyarrow.array([1, 2, 3])
+            y = pyarrow.array([1.1, 2.2, 3.3])
+            table = pyarrow.Table.from_arrays([x], ["x"])
+            table2 = table.add_column(1, pyarrow.column(pyarrow.field("y", y.type, False), numpy.array([1.1, 2.2, 3.3])))
+            assert awkward.arrow.view(table2).tolist() == [{"x": 1, "y": 1.1}, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}]
+
+    # def test_arrow_writeparquet(self):
+    #     if pyarrow is not None:
+    #         a = pyarrow.array([True, True, None, False, True])
+    #         b = pyarrow.array([0.0, 1.1, None, 3.3, 4.4])
+    #         c = pyarrow.array([None, [1.1, 2.2, None], [], None, [4.4, 5.5]])
+
+
+
+    #         table = pyarrow.Table.from_arrays
+
+    #         writer = pyarrow.parquet.ParquetWriter("tests/samples/features-0_11_1.parquet", table.schema)
+    #         writer.write_table(table)
+    #         writer.close()
