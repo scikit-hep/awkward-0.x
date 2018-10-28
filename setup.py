22d1930c6d41f5984e3581b5360aab64571c9664
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-10-28 15:46:25 +0100

    Adding test for hdf5

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 
 # Copyright (c) 2018, DIANA-HEP
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-# 
+#
 # * Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -70,7 +70,7 @@ Reference documentation
 """
 
     return description[start:stop].strip() # before + + after
-    
+
 setup(name = "awkward",
       version = get_version(),
       packages = find_packages(exclude = ["tests"]),
@@ -87,7 +87,8 @@ setup(name = "awkward",
       license = "BSD 3-clause",
       test_suite = "tests",
       install_requires = ["numpy>=1.13.1"],
-      tests_require = [],
+      setup_requires = ["pytest-runner"],
+      tests_require = ['pytest'],
       classifiers = [
           # "Development Status :: 1 - Planning",
           # "Development Status :: 2 - Pre-Alpha",
