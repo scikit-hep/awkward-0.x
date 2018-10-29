37ad6c19e0a7e12574a5e8c27082aedee39d7041
Jim Pivarski <jpivarski@gmail.com>
2018-10-29 14:47:32 -0500

    merge in fixed #23 and reinstate large-array test

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -38,8 +38,9 @@ h5py = pytest.importorskip("h5py")
 array_norm = [[1.1, 2.2, 3.3], [], [4.4, 5.5]]
 array_int = [[1, 2, 3], [], [4, 5]]
 array_f32 = [numpy.array([1.3,3.2], dtype=numpy.float32), [], numpy.array([7.6], dtype=numpy.float32)]
+array_lg = [numpy.array([1,2,3])]*10000
 
-@pytest.mark.parametrize("input_arr", (array_norm, array_int, array_f32))
+@pytest.mark.parametrize("input_arr", (array_norm, array_int, array_f32, array_lg))
 def test_read_write_hdf(tmpdir, input_arr):
     tmp_file = tmpdir / "example.h5"
 
