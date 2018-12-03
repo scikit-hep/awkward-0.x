14af90ab9552ee9ba506a22270c1a5d6e23a14d2
Ben Krikler <bek07@ic.ac.uk>
2018-12-02 22:41:49 -0700

    Fix boolean array assignment

--- awkward/array/jagged.py
+++ awkward/array/jagged.py
@@ -956,7 +956,7 @@ class JaggedArray(awkward.array.base.AwkwardArrayWithContent):
             else:
                 content = self._content != 0
 
-            out = awkward.util.numpy.empty(self._starts.shape + content.shape[1:], dtype=awkward.BOOLTYPE)
+            out = awkward.util.numpy.empty(self._starts.shape + content.shape[1:], dtype=awkward.util.BOOLTYPE)
             nonterminal = self.offsets[self.offsets != self.offsets[-1]]
             if os.name == "nt":   # Windows Numpy reduceat requires 32-bit indexes
                 nonterminal = nonterminal.astype(awkward.util.numpy.int32)
@@ -977,7 +977,7 @@ class JaggedArray(awkward.array.base.AwkwardArrayWithContent):
             else:
                 content = self._content != 0
 
-            out = awkward.util.numpy.empty(self._starts.shape + content.shape[1:], dtype=awkward.BOOLTYPE)
+            out = awkward.util.numpy.empty(self._starts.shape + content.shape[1:], dtype=awkward.util.BOOLTYPE)
             nonterminal = self.offsets[self.offsets != self.offsets[-1]]
             if os.name == "nt":   # Windows Numpy reduceat requires 32-bit indexes
                 nonterminal = nonterminal.astype(awkward.util.numpy.int32)
