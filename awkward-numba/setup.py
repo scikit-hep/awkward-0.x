461cc5edeceb6a1079f2aafe5260b2512f90985b
ASHLEY WARREN <ratin.kumar.2k@gmail.com>
2019-01-11 14:35:51 +0530

    added setup.py for awkward-numba"

---
+++ awkward-numba/setup.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018, DIANA-HEP
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os.path
+
+from setuptools import find_packages
+from setuptools import setup
+
+def get_version():
+    g = {}
+    exec(open(os.path.join("../awkward", "version.py")).read(), g)
+    return g["__version__"]
+
+def get_description():
+    description = open("README.rst", "rb").read().decode("utf8", "ignore")
+    start = description.index(".. inclusion-marker-1-5-do-not-remove")
+    stop = description.index(".. inclusion-marker-3-do-not-remove")
+
+    before = """.. image:: https://raw.githubusercontent.com/scikit-hep/awkward-array/master/docs/source/logo-500px.png
+   :alt: awkward-array
+   :target: https://github.com/scikit-hep/awkward-array
+
+|
+
+"""
+
+    after = """
+
+Tutorial
+========
+
+See the `project homepage <https://github.com/scikit-hep/awkward-array>`__ for a `tutorial <https://github.com/scikit-hep/awkward-array#tutorial>`__.
+=======================
+
+"""
+
+    return description[start:stop].strip() # before + + after
+
+setup(name = "awkward-numba",
+      version = get_version(),
+      packages = find_packages(exclude = ["tests"]),
+      scripts = [],
+      data_files = ["README.rst"],
+      description = "Numba optimisations for Awkward-array package.",
+      long_description = get_description(),
+      author = "Ratin Kumar",   
+      author_email = "ratin.kumar.2k@gmail.com",
+      maintainer = "Ratin Kumar",
+      maintainer_email = "ratin.kumar.2k@gmail.com",
+      url = "https://github.com/DumbMachine/awkward-array",
+      download_url = "https://github.com/DumbMachine/awkward-array/releases",
+      license = "BSD 3-clause",
+      test_suite = "tests",
+      install_requires = ["numba>=0.40.0","awkward>=0.7.1"],
+      setup_requires = ["pytest-runner"],
+      tests_require = ["pytest"],
+      classifiers = [
+          "Development Status :: 1 - Planning",
+          # "Development Status :: 2 - Pre-Alpha",
+          # "Development Status :: 3 - Alpha",
+          # "Development Status :: 4 - Beta",
+          # "Development Status :: 5 - Production/Stable",
+          # "Development Status :: 6 - Mature",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: MacOS",
+          "Operating System :: POSIX",
+          "Operating System :: Unix",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Information Analysis",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Software Development",
+          "Topic :: Utilities",
+          ],
+      platforms = "Any",
+      )
