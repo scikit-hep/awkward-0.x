5af0e706ad156bb642af71f4fd2ca2d3ab257945
Jim Pivarski <jpivarski@gmail.com>
2019-01-25 10:46:43 -0600

    awkward-numba must have exactly the same __init__.py as awkward

--- awkward-numba/awkward/__init__.py
+++ awkward-numba/awkward/__init__.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2019, DIANA-HEP
+# Copyright (c) 2018, DIANA-HEP
 # All rights reserved.
-#
+# 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-#
+# 
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-#
+# 
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-#
+# 
 # * Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,5 +27,23 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from awkward.array.chunked import ChunkedArray, AppendableArray
+from awkward.array.indexed import IndexedArray, SparseArray
+from awkward.array.jagged import JaggedArray
+from awkward.array.masked import MaskedArray, BitMaskedArray, IndexedMaskedArray
+from awkward.array.objects import Methods, ObjectArray, StringArray
+from awkward.array.table import Table
+from awkward.array.union import UnionArray
+from awkward.array.virtual import VirtualArray
+
+from awkward.generate import fromiter, fromiterchunks
+
+from awkward.persist import serialize, deserialize, save, load, hdf5
+
+# convenient access to the version number
+from awkward.version import __version__
+
+__all__ = ["ChunkedArray", "AppendableArray", "IndexedArray", "SparseArray", "JaggedArray", "MaskedArray", "BitMaskedArray", "IndexedMaskedArray", "Methods", "ObjectArray", "Table", "UnionArray", "VirtualArray", "StringArray", "fromiter", "fromiterchunks", "serialize", "deserialize", "save", "load", "hdf5", "__version__"]
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
