a499beb62cea77e65b46c19d21dca8eee9cabbf2
Jim Pivarski <jpivarski@gmail.com>
2019-01-25 13:20:20 -0600

    Actually implement the _argminmax accelerator

--- awkward-numba/awkward/numba/array/jagged.py
+++ awkward-numba/awkward/numba/array/jagged.py
@@ -28,8 +28,66 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import numba
+
 import awkward.array.jagged
 from .base import NumbaMethods
 
+@numba.jit(nopython=True)
+def _argminmax_fillmin(starts, stops, content, output):
+    k = 0
+    for i in range(len(starts)):
+        if stops[i] != starts[i]:
+            best = content[starts[i]]
+            bestj = 0
+            for j in range(starts[i] + 1, stops[i]):
+                if content[j] < best:
+                    best = content[j]
+                    bestj = j - starts[i]
+            output[k] = bestj
+            k += 1
+
+@numba.jit(nopython=True)
+def _argminmax_fillmax(starts, stops, content, output):
+    k = 0
+    for i in range(len(starts)):
+        if stops[i] != starts[i]:
+            best = content[starts[i]]
+            bestj = 0
+            for j in range(starts[i] + 1, stops[i]):
+                if content[j] > best:
+                    best = content[j]
+                    bestj = j - starts[i]
+            output[k] = bestj
+            k += 1
+
 class JaggedArrayNumba(awkward.array.jagged.JaggedArray, NumbaMethods):
-    pass
+    def _argminmax(self, ismin):
+        if len(self._starts) == len(self._stops) == 0:
+            return self.copy()
+
+        if len(self._content.shape) != 1:
+            raise ValueError("cannot compute arg{0} because content is not one-dimensional".format("min" if ismin else "max"))
+
+        # subarray with counts > 0 --> counts = 1
+        counts = (self.counts != 0).astype(self.INDEXTYPE)
+        # offsets for these 0 or 1 counts (involves a cumsum)
+        offsets = self.counts2offsets(counts)
+        # starts and stops derived from offsets and reshaped to original starts and stops (see specification)
+        starts, stops = offsets[:-1], offsets[1:]
+        starts.reshape(self._starts.shape[:-1] + (-1,))
+        stops.reshape(self._starts.shape[:-1] + (-1,))
+
+        # content to fit the new offsets
+        content = awkward.util.numpy.empty(offsets[-1], dtype=self.INDEXTYPE)
+
+        # fill the new content
+        if ismin:
+            _argminmax_fillmin(self._starts.reshape(-1), self._stops.reshape(-1), self._content, content)
+        else:
+            _argminmax_fillmax(self._starts.reshape(-1), self._stops.reshape(-1), self._content, content)
+
+        return self.copy(starts=starts, stops=stops, content=content)
+
+    def _argminmax_general(self, ismin):
+        raise RuntimeError("helper function not needed in JaggedArrayNumba")
