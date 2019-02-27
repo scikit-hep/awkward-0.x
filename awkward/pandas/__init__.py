6e66b12fe08a8de4658bbdadcd902fb8b20e4ba8
Michael Hedges <mhedges@hawaii.edu>
2019-02-27 09:51:57 -0600

    [WIP] creating __init__ file to load accessor, ExtensionArray, and ExtensionDType

---
+++ awkward/pandas/__init__.py
@@ -0,0 +1,11 @@
+from .base import (
+        AwkwardArray,
+        AwkwardType,
+        AwkwardAccessor,
+        )
+
+__all__ = [
+        'AwkwardAccessor',
+        'AwkwardType',
+        'AwkwardArray',
+        ]
