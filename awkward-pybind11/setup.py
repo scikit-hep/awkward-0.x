4b90cef573d42185065e820064b5e01c9039cab6
cescott <chuckescott@comcast.net>
2019-04-10 14:38:04 -0400

    First .pyd commit
    Changed .gitignore to allow .pyd files, created folder architecture, put
    in JaggedArray.pyd, and also included the .cpp origin in src folder

---
+++ awkward-pybind11/setup.py
@@ -0,0 +1,56 @@
+import os.path
+
+from setuptools import find_packages
+from setuptools import setup
+
+def get_version():
+    g = {}
+    exec(open(os.path.join("..", "awkward", "version.py")).read(), g)
+    return g["__version__"]
+
+setup(name = "awkward-pybind11",
+      version = get_version(),
+      packages = find_packages(exclude = ["tests"]),
+      scripts = [],
+      description = "Connect awkward-arrays to C++ using pybind11.",
+      long_description = "",
+      author = "Charles Escott",
+      author_email = "charlescescott@gmail.com",
+      maintainer = "Jim Pivarski (IRIS-HEP)",
+      maintainer_email = "pivarski@princeton.edu",
+      url = "https://github.com/scikit-hep/awkward-array",
+      download_url = "https://github.com/scikit-hep/awkward-array/releases",
+      license = "BSD 3-clause",
+      test_suite = "tests",
+      install_requires = ["awkward==" + get_version(), "pybind11"],
+      setup_requires = ["pytest-runner"],
+      tests_require = ["pytest"],
+      classifiers = [
+          "Development Status :: 1 - Planning",
+          # "Development Status :: 2 - Pre-Alpha",
+          # "Development Status :: 3 - Alpha",
+          # "Development Status :: 4 - Beta",
+          # "Development Status :: 5 - Production/Stable",
+          # "Development Status :: 6 - Mature",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: MacOS",
+          "Operating System :: POSIX",
+          "Operating System :: Unix",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Information Analysis",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Software Development",
+          "Topic :: Utilities",
+          ],
+      platforms = "Any",
+      )
---
+++ awkward-pybind11/src/JaggedArray.cpp
@@ -0,0 +1,122 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+// Temporary test variables
+int offsets[5] = { 0, 2, 4, 4, 7 };
+int offsetLen = 5;
+int parents[7] = { 0, 0, 0, 0, 0, 0, 0 };
+int parentLen = 7;
+
+/*-----------------------------------------------------
+Function Name:	offsets2parents
+
+Description:	Populates a properly-sized parents
+				array based on the values from an
+				offsets array.
+
+Inputs:			Nothing technically, until I find out
+				how to pass arrays by reference and
+				modify their contents in a function
+
+Outputs:		-- (array should be modified)
+
+Note:			I modeled this function after
+				_offsets2parents_fill() in the jagged
+				file from awkward-numba
+-----------------------------------------------------*/
+void offsets2parents() {
+	int j = 0;
+	int k = -1;
+	for (int i = 0; i < offsetLen; i++) {
+		while (j < offsets[i]) {
+			parents[j] = k;
+			j += 1;
+		}
+		k += 1;
+	}
+}
+
+
+/*-----------------------------------------------------
+Function Name:	toString
+
+Description:	Converts an array into a string in the
+				format '{ 1, 2, 3 }'
+
+Inputs:			int input[]		The array to be converted
+				int length		The length of the array
+
+Outputs:		string str		Converted product
+
+Note:			This is another temporary piece of the
+				file just to be used for test purposes
+-----------------------------------------------------*/
+std::string toString(int input[], int length) {
+	std::string str = "{ ";
+	for (int i = 0; i < length - 1; i++) {
+		str += std::to_string(input[i]);
+		str += ", ";
+	}
+	if (length > 0) {
+		str += std::to_string(input[length - 1]);
+		str += " ";
+	}
+	str += "}";
+	return str;
+}
+
+
+/*-----------------------------------------------------
+Function Name:	showOffsets
+
+Description:	Calls toString() for the offsets array
+				so it can be displayed in the python
+				shell
+
+Inputs:			--
+
+Outputs:		string offsets
+
+Note:			Still another test function
+-----------------------------------------------------*/
+std::string showOffsets() {
+	return toString(offsets, offsetLen);
+}
+
+
+/*-----------------------------------------------------
+Function Name:	showParents
+
+Description:	Calls toString() for the parents array
+				so it can be displayed in the python
+				shell
+
+Inputs:			--
+
+Outputs:		string parents
+
+Note:			Still another test function
+-----------------------------------------------------*/
+std::string showParents() {
+	return toString(parents, parentLen);
+}
+
+/*-----------------------------------------------------
+Note:			This section is what binds the C++
+				code to python. It will normally be in
+				a separate file, but for convenience
+				and simplicity I've left it here.
+-----------------------------------------------------*/
+PYBIND11_MODULE(JaggedArray, m) {
+	m.doc() = "Pybind11 functions for jagged arrays";
+	
+	m.def("offsets2parents", &offsets2parents, 
+		"Populates a properly-sized parents array based on the values from an offsets array.");
+	m.def("toString", &toString,
+		"Converts an array into a string in the format \'{ 1, 2, 3 }\'");
+	m.def("showOffsets", &showOffsets,
+		"Calls toString() for the offsets array so it can be displayed in the python shell");
+	m.def("showParents", &showParents,
+		"Calls toString() for the parents array so it can be displayed in the python shell");
+}
