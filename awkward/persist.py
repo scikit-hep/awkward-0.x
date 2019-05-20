c2c033b0e454a019e2c71ac7e0a1feb57aa302e7
Jim Pivarski <jpivarski@gmail.com>
2019-05-20 13:51:52 -0500

    looks ready for uproot lazy arrays

--- awkward/persist.py
+++ awkward/persist.py
@@ -42,6 +42,7 @@ whitelist = [
         ["awkward.persist", "*"],
         ["awkward.arrow", "_ParquetFile", "fromjson"],
         ["uproot_methods.classes.*"],
+        ["uproot.tree._LazyFiles"],
         ["uproot.tree._LazyTree"],
         ["uproot.tree._LazyBranch"],
     ]
