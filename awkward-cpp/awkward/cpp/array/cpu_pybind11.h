5c66059fb2ffd5e7c2db5cafa4fbadde0a6ad27a
EscottC <charlescescott@gmail.com>
2019-07-12 14:27:13 -0700

    fixing travis errors attempt #2

--- awkward-cpp/awkward/cpp/array/cpu_pybind11.h
+++ awkward-cpp/awkward/cpp/array/cpu_pybind11.h
@@ -24,38 +24,38 @@ struct c_array py2c(py::array input) {
 }
 
 int makeIntNative_CPU(py::array input) {
-    if (!checkInt_CPU(py2c(input))) {
+    if (!checkInt_CPU(&py2c(input))) {
         throw std::invalid_argument("Argument must be an int array");
     }
-    if (!makeNative_CPU(py2c(input))) {
+    if (!makeNative_CPU(&py2c(input))) {
         throw std::invalid_argument("Error in cpu_methods.h::makeNative_CPU");
     }
     return 1;
 }
 
 int getMax_CPU(py::array input, std::int8_t* max) {
-    if (!getMax_8bit(py2c(input), 0, 0, max)) {
+    if (!getMax_8bit(&py2c(input), 0, 0, max)) {
         throw std::invalid_argument("Error in cpu_methods.h::getMax_8bit");
     }
     return 1;
 }
 
 int getMax_CPU(py::array input, std::int16_t* max) {
-    if (!getMax_16bit(py2c(input), 0, 0, max)) {
+    if (!getMax_16bit(&py2c(input), 0, 0, max)) {
         throw std::invalid_argument("Error in cpu_methods.h::getMax_16bit");
     }
     return 1;
 }
 
 int getMax_CPU(py::array input, std::int32_t* max) {
-    if (!getMax_32bit(py2c(input), 0, 0, max)) {
+    if (!getMax_32bit(&py2c(input), 0, 0, max)) {
         throw std::invalid_argument("Error in cpu_methods.h::getMax_32bit");
     }
     return 1;
 }
 
 int getMax_CPU(py::array input, std::int64_t* max) {
-    if (!getMax_64bit(py2c(input), 0, 0, max)) {
+    if (!getMax_64bit(&py2c(input), 0, 0, max)) {
         throw std::invalid_argument("Error in cpu_methods.h::getMax_64bit");
     }
     return 1;
