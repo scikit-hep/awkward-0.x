87dbf601013ccf2cd68b1d2df5b8e8730d29b7a5
Ben Krikler <bek07@ic.ac.uk>
2019-11-19 08:11:30 +0100

    Add absolute_import needed for python2

--- awkward/pandas.py
+++ awkward/pandas.py
@@ -2,6 +2,7 @@
 
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-array/blob/master/LICENSE
 
+from __future__ import absolute_import
 import pandas
 import pandas.api.extensions
 
